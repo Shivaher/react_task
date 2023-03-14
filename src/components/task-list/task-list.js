293653592fee3c1235f6def1cd79b75c3e6613ea
Dmitrii <D1Fol@yandex.ru>
2023-03-14 22:33:26 +0500

    timer
    1

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -10,23 +10,33 @@ export default class TaskList extends React.Component {
     todos: [],
     onDeleted: () => {},
     onCompltedTask: () => {},
+    stopTimer: () => {},
+    startTimer: () => {},
   }
 
   static propTypes = {
     todos: PropTypes.array,
     onDeleted: PropTypes.func,
     onCompltedTask: PropTypes.func,
+    stopTimer: PropTypes.func,
+    startTimer: PropTypes.func,
   }
 
   render() {
-    const { todos, onDeleted, onCompltedTask } = this.props
+    const { todos, onDeleted, onCompltedTask, startTimer, stopTimer } = this.props
 
     const elements = todos.map((item) => {
       const { id, ...itemProps } = item
 
       return (
         <li key={id}>
-          <TaskItem {...itemProps} onCompltedTask={() => onCompltedTask(id)} onDeleted={() => onDeleted(id)} />
+          <TaskItem
+            {...itemProps}
+            onCompltedTask={() => onCompltedTask(id)}
+            onDeleted={() => onDeleted(id)}
+            stopTimer={() => stopTimer(id)}
+            startTimer={() => startTimer(id)}
+          />
         </li>
       )
     })
--- src/index.js
+++ src/index.js
@@ -11,21 +11,50 @@ import './index.css'
 class App extends React.Component {
   maxID = 100
   state = {
-    todoData: [
-      this.createTodoItem('Drink Coffe'),
-      this.createTodoItem('Go Gym'),
-      this.createTodoItem('Go to Bathroom'),
-    ],
+    todoData: [],
     filter: 'all',
   }
 
-  createTodoItem(label) {
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(({ todoData }) => {
+        let newArr = todoData.map((el) => {
+          if (el.time === 0) {
+            return el
+          }
+          if (!el.pause) {
+            el.time = el.time - 1
+          }
+          return el
+        })
+        return {
+          todoData: newArr,
+        }
+      })
+    }, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  createTodoItem(label, time) {
     return {
       label,
+      time: time,
       done: false,
       created: new Date(),
       id: this.maxID++,
+      pause: false,
     }
+  }
+
+  addItem = (text, time) => {
+    const newItem = this.createTodoItem(text, time)
+    this.setState(({ todoData }) => {
+      const newArray = [...todoData, newItem]
+      return { todoData: newArray }
+    })
   }
 
   deleteItem = (id) => {
@@ -43,27 +72,12 @@ class App extends React.Component {
   compltedTask = (id) => {
     this.setState(({ todoData }) => {
       const idx = todoData.findIndex((el) => el.id === id)
-      const doneTasks = [{ ...todoData[idx], done: !todoData[idx].done }]
-      const newData = [...todoData.slice(0, idx), ...doneTasks, ...todoData.slice(idx + 1)]
+      const oldItem = todoData[idx]
+      const newItem = { ...oldItem, done: !oldItem.done, pause: !oldItem.pause }
+      const newArray = [...todoData.slice(0, idx), newItem, ...todoData.slice(idx + 1)]
       return {
-        todoData: newData,
+        todoData: newArray,
       }
-    })
-  }
-
-  onToggleDone = (id) => {
-    this.setState(({ todoData }) => {
-      return {
-        todoData: this.toggleProperty(todoData, id, 'done'),
-      }
-    })
-  }
-
-  addItem = (text) => {
-    const newItem = this.createTodoItem(text)
-    this.setState(({ todoData }) => {
-      const newArray = [...todoData, newItem]
-      return { todoData: newArray }
     })
   }
 
@@ -90,6 +104,28 @@ class App extends React.Component {
     })
   }
 
+  stopTimer = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const newObj = [{ ...todoData[idx], pause: true }]
+      const newArray = [...todoData.slice(0, idx), ...newObj, ...todoData.slice(idx + 1)]
+      return {
+        todoData: newArray,
+      }
+    })
+  }
+
+  startTimer = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((el) => el.id === id)
+      const newObj = [{ ...todoData[idx], pause: false }]
+      const newArray = [...todoData.slice(0, idx), ...newObj, ...todoData.slice(idx + 1)]
+      return {
+        todoData: newArray,
+      }
+    })
+  }
+
   render() {
     const { todoData, filter } = this.state
 
@@ -99,17 +135,25 @@ class App extends React.Component {
     const todoCount = todoData.length - doneCount
 
     return (
-      <div>
+      <section className="todoapp">
         <Header />
-        <NewTaskForm onItemAdded={this.addItem} />
-        <TaskList todos={visibleTask} onCompltedTask={this.compltedTask} onDeleted={this.deleteItem} />
-        <Footer
-          itemLeft={todoCount}
-          filter={filter}
-          onFilterChange={this.onFilterChange}
-          onClearCompleted={this.onClearCompleted}
-        />
-      </div>
+        <NewTaskForm addItem={this.addItem} />
+        <section className="main">
+          <TaskList
+            todos={visibleTask}
+            onCompltedTask={this.compltedTask}
+            onDeleted={this.deleteItem}
+            stopTimer={this.stopTimer}
+            startTimer={this.startTimer}
+          />
+          <Footer
+            itemLeft={todoCount}
+            filter={filter}
+            onFilterChange={this.onFilterChange}
+            onClearCompleted={this.onClearCompleted}
+          />
+        </section>
+      </section>
     )
   }
 }
